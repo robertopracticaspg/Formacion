f57c3da93dce1690f9ad40868152f3433ff75523
robertopracticaspg <93863224+robertopracticaspg@users.noreply.github.com>
2023-06-01 11:11:48 +0200

    Update FormacionServiceImpl.java

--- Ej022CursoCliente/src/main/java/com/curso/service/FormacionServiceImpl.java
+++ Ej022CursoCliente/src/main/java/com/curso/service/FormacionServiceImpl.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-
 import com.curso.model.CursoAux;
 import com.curso.model.Formacion;
 
@@ -39,38 +38,33 @@ public class FormacionServiceImpl implements FormacionService {
 		}
 		return formaciones;
 	}
-			
 
-			@Override
-			public void altaCurso(Formacion form, int codCurso) {
+	@Override
+	public void altaCurso(Formacion form, int codCurso) {
 
-			    CursoAux curso = new CursoAux();
-			    
+		CursoAux curso = new CursoAux();
 
-			    List<CursoAux> listaCur = Arrays.asList(template.getForObject(url, CursoAux[].class));
-			    
-			    boolean cursoExiste = false; //Para saber si el curso existe o no
-			    
-			    for (CursoAux curso2 : listaCur) {
-			        if (curso2.getNombre().equals(curso.getNombre())) {
-			        	cursoExiste = true;
-			            break;
-			        }
-			    }
-			    
-			    if (!cursoExiste) {
-			    	
-			    	curso.setDuracion(form.getAsignaturas() * 10);
-				    curso.setCodCurso(codCurso);
-				    curso.setNombre(form.getCurso());
-				    curso.setPrecio((int) form.getPrecio());
-			        template.postForLocation(url, curso);
-			    }
-			    template.postForLocation(url, curso);
+		List<CursoAux> listaCur = Arrays.asList(template.getForObject(url, CursoAux[].class));
+
+		boolean cursoExiste = false; // Para saber si el curso existe o no
+
+		for (CursoAux curso2 : listaCur) {
+			if (curso2.getNombre().equals(curso.getNombre())) {
+				cursoExiste = true;
+				break;
 			}
-
-			
-		   
 		}
-	
+
+		if (!cursoExiste) {
+
+			curso.setDuracion(form.getAsignaturas() * 10);
+			curso.setCodCurso(codCurso);
+			curso.setNombre(form.getCurso());
+			curso.setPrecio((int) form.getPrecio());
+			template.postForLocation(url, curso);
+		}
+		template.postForLocation(url, curso);
+	}
+
+}
 
